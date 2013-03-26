5e62bdd9608e21c2839e4a3c2b2b27e2dcd288d5
rmackenzie <ron.mackenzie+gh@gmail.com>
2013-03-26 10:11:07 -0400

    liberty counter appears to pass all tests

---
+++ README.md
@@ -0,0 +1,10 @@
+GO IN GO
+
+Software for playing go, written in the language Go. 
+
+Intended features:
+Selection of ruleset and time settings
+Online multiplayer via peer to peer connection
+Graphical interface
+
+Currently non-functional, but will update with time
--- board.go
+++ board.go
@@ -6,6 +6,9 @@ package main
 
 import "fmt"
 
+/**
+* ///////////////////////////////////////////////////// START BOARD METHODS /////////////////////////////////////////
+*/
 type Board struct{
 	board [][]int
 }
@@ -25,7 +28,7 @@ func (f Board) printBoard() {
 	for i := range f.board {
 		for j := range f.board[i] {
 			fmt.Printf("[")
-			fmt.Printf(string(f.board[i][j]))
+			fmt.Print(f.board[i][j])
 			fmt.Printf("]")
 		}
 		fmt.Printf("\n")
@@ -39,12 +42,12 @@ func (f Board) printBoard() {
 *	TODO represent p as object of type Player later
 *	Returns true if move is successful, false if invalid move
 */
-func (f Board) move(x int, y int, p int) (bool) {
-	if !f.validMove(x,y,p) {
+func (f Board) move(pair Pair, p int) (bool) {
+	if !f.validMove(pair,p) {
 		return false
 	} 
 
-	f.board[x-1][y-1] = p
+	f.board[pair.x-1][pair.y-1] = p
 	//check liberties
 	//check for ko
 	
@@ -54,24 +57,157 @@ func (f Board) move(x int, y int, p int) (bool) {
 /**
 * Checks the liberties of a stone, including those of any group it is part of
 */
-func (f Board) checkLibs(x int, y int, p int) int {
-	var cnt = 0
-	if f.board[x-1][y] == 0 {
-		cnt++
-	}
+func (f Board) checkLibs(pair Pair, p int) int {
+
+	stack := newStack()
+	checkedPairs := newArrayList()
+	cnt := 0
 	//TODO: IMPLEMENTATION
+	// first push every terminal stone into a stack
+	// then sum the liberties of all those stones
+
+	stack.push(newPair(pair.x,pair.y))
+	for !stack.isEmpty() {
+		checkPair := stack.pop()
+		if !checkedPairs.member(*(checkPair)) {
+			if f.board[checkPair.x-1][checkPair.y] == p {
+				stack.push(newPair(checkPair.x-1, checkPair.y))
+			} else if f.board[checkPair.x-1][checkPair.y] == 0 {
+				cnt++
+			}
+			if f.board[checkPair.x][checkPair.y-1] == p {
+				stack.push(newPair(checkPair.x, checkPair.y-1))
+			} else if f.board[checkPair.x][checkPair.y-1] == 0 {
+				cnt++
+			}
+			if f.board[checkPair.x+1][checkPair.y] == p {
+				stack.push(newPair(checkPair.x+1, checkPair.y))
+			} else if f.board[checkPair.x+1][checkPair.y] == 0 {
+				cnt++
+			}
+			if f.board[checkPair.x][checkPair.y+1] == p {
+				stack.push(newPair(checkPair.x, checkPair.y+1))
+			} else if f.board[checkPair.x][checkPair.y+1] == 0 {
+				cnt++
+			}
+		}
+	}
 	return cnt
 }
 
 /**
 * Checks if a given move is valid
 */
-func (f Board) validMove(x int, y int, p int) bool {
+func (f Board) validMove(pair Pair, p int) bool {
 	//TODO: IMPLEMENTATION
 	return false
 }
 
+/**
+* Will use stacks for counting liberties
+* /////////////////////////////////////////////////////// START STACK METHODS //////////////////////////////////////////////////
+*/
+type Stack struct{
+	array []*Pair
+	last int
+}
+
+func newStack() *Stack {
+	f := new(Stack)
+	f.array = make([]*Pair,5)
+	f.last = 0
+	return f
+}
+
+func (f Stack) isEmpty() bool {
+	return f.last==0
+}
+
+func (f *Stack) push(pair *Pair) bool {
+	if len(f.array) == cap(f.array) { //need to expand the stack
+		var newArray = make([]*Pair, len(f.array), (cap(f.array)+1)*2)
+		copy(newArray, f.array)
+		f.array = newArray
+	}
+	f.last++
+	f.array[f.last] = pair
+	return true
+}
+
+func (f *Stack) pop() (*Pair) {
+	if f.last == 0 {
+		return nil
+	}
+	tmp := f.array[f.last]
+	f.array[f.last] = nil
+	f.last--
+	return tmp
+}
+
+/**
+* Represent pairs on the board, used instead of passing x,y references to every method
+* //////////////////////////////////////////////////////// START PAIR METHODS /////////////////////////////////////////////////
+*/
+type Pair struct{
+	x int
+	y int
+}
+
+func newPair(x int, y int) *Pair {
+	f := new(Pair)
+	f.x = x
+	f.y = y
+	return f
+}
+
+func (f *Pair) Print() {
+	fmt.Printf("%d %d\n", f.x, f.y)
+}
+
+func (f Pair) equals(pair Pair) bool {
+	return (f.x == pair.x && f.y == pair.y)
+}
+
+/**
+* Use lists for keeping track of things counted
+* /////////////////////////////////////////////////////// START ARRAYLIST METHODS ///////////////////////////////////////////////
+*/
+
+type ArrayList struct{
+	array []Pair
+	last int
+}
+
+func newArrayList() *ArrayList {
+	f := new(ArrayList)
+	f.array = make([]Pair, 10)
+	f.last = 0
+	return f
+}
+
+func (f ArrayList) member(pair Pair) bool {
+	for i := range f.array {
+		if pair.equals(f.array[i]) {
+			return true
+		}
+	}
+	return false
+}
+
+func (f *ArrayList) add(pair Pair) {
+	if f.last == cap(f.array) {
+		newArray := make([]Pair, len(f.array), (cap(f.array)+1)*2)
+		copy(newArray, f.array)
+		f.array = newArray
+	}
+	f.last++
+	f.array[f.last] = pair
+}
+
 func main() {
 	board := newBoard()
+	board.board[2][2] = 1
 	board.printBoard()
+	stone := newPair(2,2)	
+	fmt.Println(board.checkLibs(*stone, 1))
 }
